80502f656d359fff01bbc3e84ba4db379b9e1774
Sander Decoster <sander.decoster@kbc.be>
2016-04-29 15:16:45 +0200

    Updated example

--- src/components/basic.component.ts
+++ src/components/basic.component.ts
@@ -1,10 +1,9 @@
 import {Component} from 'angular2/core'
-import 'rxjs/Rx'
-import {Observable} from "rxjs/Observable";
+import {Observable} from "rxjs/Rx";
 
 @Component({
   selector: 'basic',
-  template: `<h2>Basic example</h2>`
+  template: `<h2>Basic example</h2><p>See console</p>`
 })
 
 export class BasicComponent {
@@ -14,11 +13,17 @@ export class BasicComponent {
     // Create the observable
     let observable$ = new Observable((observer) => {
       observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.next(4);
+      observer.next(5);
 
       observer.complete();
     });
 
     observable$
+      .map((x: number) => x * 3)
+      .filter(x => x % 2 === 0)
       .subscribe(
         (value) => console.log(value),
         (error) => console.log('error = ', error),
